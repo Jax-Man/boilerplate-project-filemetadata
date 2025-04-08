d98ee5a226e8af1275cf0a9b483cee58f7fec7e4
Jax-Man <amcli8079@gmail.com>
2025-04-08 10:14:25 -0400

    initial commit
    completed project done for the backend api certification with freeCodeCamp

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,7 +13,11 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.route('/api/fileanalyse').post(upload.single('upfile'), function (req, res) {
+  const file = req.file;
+  console.log(file);
+  res.json({ name: file.originalname, type: file.mimetype, size: file.size });
+});
 
 
 const port = process.env.PORT || 3000;
